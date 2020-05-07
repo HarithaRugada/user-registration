5402c87c3c41d2deb57661597bb987dfc17fcb74
HarithaRugada <haritharugada@gmail.com>
2020-05-07 16:27:32 +0530

    added usecase1

--- user.sh
+++ user.sh
@@ -1,2 +1,10 @@
-#! /bin/bash/ -x
-echo "Program for User Details Validation"
+#! /bin/bash/
+echo "Program for user details validation"
+read -p "enter the first name > " fname
+
+if [[ $fname =~ ^([[:upper:]][[:lower:]]{2,})$ ]]
+then
+        echo "valid first name"
+else
+        echo "invalid first name"
+fi
